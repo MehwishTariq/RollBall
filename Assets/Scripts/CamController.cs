0598bcfebe877e548af0aa614805c0c12cb3d8dd
MEHWISH TARIQ <mickeyminnie92@hotmail.com>
2020-07-16 18:10:14 +0500

    Added files

---
+++ Assets/Scripts/CamController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate is called once per frame after items in update are processed
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    public Text countText,winText;
+
+    private int count;
+    private Rigidbody rb;
+
+    //initialization when game starts
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        winText.text = "";
+        SetCountText();
+    }
+    //physics implementation functions come here
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(move*speed);
+    }
+
+    //check collision to deactivate the collectables
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUps"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count == 14)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
